9a6a5009de375fb277103aebe88efe1d877eb5c3
Hao Chen <501018786@qq.com>
2023-12-05 09:06:53 +0800

    lab02: part one

--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -4,7 +4,9 @@
 #include <string.h>
 #include "lfsr.h"
 
-void lfsr_calculate(uint16_t *reg) {
+void lfsr_calculate(uint16_t *reg)
+{
     /* YOUR CODE HERE */
+    unsigned new_bit = (*reg ^ (*reg >> 2) ^ (*reg >> 3) ^ (*reg >> 5)) & 1;
+    *reg = (*reg >> 1) | (new_bit << 15);
 }
-
